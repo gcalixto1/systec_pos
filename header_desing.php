fa33b13fb4dc2b45db163fe0f099406aa1c38d5d
gcalixto1 <alex.calix1992@gmail.com>
2025-05-07 10:30:56 -0600

    header

--- header_desing.php
+++ header_desing.php
@@ -6,10 +6,14 @@
 
 
 <link href="assets/css/style_login.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/Datatables/datatables.min.css" />
+<!--datables estilo bootstrap 4 CSS-->
+<link rel="stylesheet" type="text/css" href="assets/Datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
 
 <script src="assets/vendor/jquery/jquery.min.js"></script>
 <!-- <script src="https://cdn.datatables.net/2.0.3/js/dataTables.bootstrap5.js "></script> -->
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
 <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
 <script src="assets/vendor/php-email-form/validate.js"></script>
 <script src="assets/vendor/venobox/venobox.min.js"></script>
@@ -21,4 +25,14 @@
 <script type="text/javascript" src="assets/js/jquery.datetimepicker.full.min.js"></script>
 <script type="text/javascript" src="assets/font-awesome/js/all.min.js"></script>
 <script type="text/javascript" src="assets/js/jquery-te-1.4.0.min.js" charset="utf-8"></script>
-<script src="js/departamentos.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+<script type="text/javascript" src="assets/DataTables/datatables.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- para usar botones en datatables JS -->
+<script src="assets/DataTables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>
+<script src="assets/DataTables/JSZip-2.5.0/jszip.min.js"></script>
+<script src="assets/DataTables/pdfmake-0.1.36/pdfmake.min.js"></script>
+<script src="assets/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
+<script src="assets/DataTables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
